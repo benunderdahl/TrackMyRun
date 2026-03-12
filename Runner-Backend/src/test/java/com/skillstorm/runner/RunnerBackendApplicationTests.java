3424dbf510cf272f64897a1c4658d4a7047e05f5
benunderdahl <benjamin.underdahl@proton.me>
2026-03-12 09:08:51 -0500

    added backend files for spring boot with dependencies

---
+++ Runner-Backend/src/test/java/com/skillstorm/runner/RunnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
