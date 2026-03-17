6b6f5a87690bc8bfcb1cd6cacf0db6eb5507780e
Code Classroom <benjamin.underdahl@proton.me>
2026-03-17 14:58:38 -0500

    added front end components, services and models templates

---
+++ Runner-Frontend/src/app/components/runs/runs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-runs',
+  imports: [],
+  templateUrl: './runs.html',
+  styleUrl: './runs.css',
+})
+export class Runs {
+
+}
---
+++ Runner-Frontend/src/app/components/shoes/shoes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoes',
+  imports: [],
+  templateUrl: './shoes.html',
+  styleUrl: './shoes.css',
+})
+export class Shoes {
+
+}
